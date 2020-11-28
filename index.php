867f9e3b3193705de42b7a35148a51fa42b46501
FrostWolfX <stukalov.blog@gmail.com>
2020-11-28 19:13:47 +0300

    fix react v 0

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 $uri = $_SERVER['REQUEST_URI'];
 require_once __DIR__.'/react-intro/build/index.html';
 require_once __DIR__.'/scr/curl.php';
+
+?>
+	<!-- Load React. -->
+	<!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+	<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+	<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+	<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+<?php
+
+
 /*if($uri === '/')
     require './s/index.html';*/
 
