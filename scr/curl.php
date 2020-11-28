874b9ec53cc2e3f22657254d086facbe159e3bc0
FrostWolfX <stukalov.blog@gmail.com>
2020-11-28 13:32:11 +0300

    Реализация точки входа в curl.php и реализация класса searchKeyWords

--- scr/curl.php
+++ scr/curl.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '/searchKeyWords.php';
+
 function setCache($content, $cacheId)
 {
 	if ($content == '') {
@@ -48,32 +50,40 @@ function curlLoad($url, $cash = 0)
 	return $content;
 }
 
-$url = 'https://spb.hh.ru/search/resume?exp_period=all_time&logic=normal&pos=full_text&fromSearchLine=true&schedule=fullDay&clusters=True&area=2&order_by=relevance&no_magic=False&ored_clusters=True&st=resumeSearch&text=java';
-$content = curlLoad($url, $cash = 3600);
-$pattern = '~<div data-qa="resume-serp__results-search">.+<div class="bloko-gap bloko-gap_top">~isU';
-preg_match($pattern, $content, $matches);
-$innerContent = $matches[0];
-preg_match_all('~href="(.*)">(.*)</a>~isU', $innerContent, $links);
 
-$viewLink = $links[1];//ссылка на резюме
-$viewName = $links[2];//название
-$summaries = [];
+function arrayResume($content)
+{
+	$pattern = '~<div data-qa="resume-serp__results-search">.+<div class="bloko-gap bloko-gap_top">~isU';
+	preg_match($pattern, $content, $matches);
+	$innerContent = $matches[0];
+	preg_match_all('~href="(.*)">(.*)</a>~isU', $innerContent, $links);
 
-for ($i = 0; $i < count($viewLink); $i++) {
-	echo '<a href="https://hh.ru' . $viewLink[$i] . '">' . $viewName[$i] . '</a><br>';
-	//парсинг резюме по ссылкам
-	$summaries_links = curlLoad('https://spb.hh.ru' . $viewLink[$i], $cash = 3600);
-	$pattern = '~<div class="resume-applicant">(?P<resume>.*)?</div>\s*</div>\s*</div>\s*</div></div>\s*</div>\s*</div>\s*</div>\s*</div>\s*</div>~isU';
-	preg_match($pattern, $summaries_links, $matches);
-	$summaries['https://spb.hh.ru' . $viewLink[$i]] = $matches['resume'];
+	$viewLink = $links[1];//ссылка на резюме
+	$viewName = $links[2];//название
+	$summaries = [];
+
+	for ($i = 0; $i < count($viewLink); $i++) {
+		echo '<a href="https://hh.ru' . $viewLink[$i] . '">' . $viewName[$i] . '</a><br>';
+		//парсинг резюме по ссылкам
+		$summaries_links = curlLoad('https://spb.hh.ru' . $viewLink[$i], $cash = 3600);
+		$pattern = '~<div class="resume-applicant">(?P<resume>.*)?</div>\s*</div>\s*</div>\s*</div></div>\s*</div>\s*</div>\s*</div>\s*</div>\s*</div>~isU';
+		preg_match($pattern, $summaries_links, $matches);
+		$summaries['https://spb.hh.ru' . $viewLink[$i]] = $matches['resume'];
+	}
+
+	return $summaries;
 }
 
-echo "<pre>";
-//print_r($summaries);
-echo "</pre>";
-echo '<hr>';
-echo '<hr>';
-echo '<hr>';
-return $summaries;
+//точка входа
+$url = 'https://spb.hh.ru/search/resume?exp_period=all_time&logic=normal&pos=full_text&fromSearchLine=true&schedule=fullDay&clusters=True&area=2&order_by=relevance&no_magic=False&ored_clusters=True&st=resumeSearch&text=java';
+$keywords = ['SQL', 'Java', 'MySQL', 'REST'];
 
+//получаем весь контент страницы
+$content = curlLoad($url, $cash = 3600);
+//получаем БД резюме по отдельности в массиве
+$resumeArray = arrayResume($content);
 
+$searchKeyWorld = new SearchKeyWorld($resumeArray,$keywords);
+$key = $searchKeyWorld->keywords();
+
+print_r($key);
--- scr/searchKeyWords.php
+++ scr/searchKeyWords.php
@@ -1,29 +1,72 @@
 <?php
-$summaries = require_once __DIR__ . '/curl.php';
 
-function keywords(array $summaries, array $keywords = ['SQL', 'Java', 'MySQL', 'REST'])
+class SearchKeyWorld
 {
-	foreach ($summaries as $link => $resume) {
-		for ($i = 0; $i < count($keywords); $i++) {
+	private $summaries;
+	private $keywords;
 
-			$search = $keywords[$i];
-			$replace = '<span style="font-size: 200%; font-family: monospace; background: lightgreen">' . $search . '</span>';
-			if ($i === 0) {
-				$subject = $resume;
-			} else {
-				$subject = $query;
-			}
+	public function __construct(array $summaries, array $keywords)
+	{
+		$this->summaries = $summaries;
+		$this->keywords = $keywords;
+	}
 
-			$query = str_ireplace($search, $replace, $subject, $count);
+	/**
+	 * @return array
+	 */
+	private function getSummaries(): array
+	{
+		return $this->summaries;
+	}
 
-			if ($count > 0) {
-				$summaries[$link] = $query;
-				$keysFound[] = $search;
+	/**
+	 * @return array
+	 */
+	private function getKeywords(): array
+	{
+		return $this->keywords;
+	}
+
+	/**
+	 * @param array $summaries
+	 */
+	public function setSummaries(array $summaries): void
+	{
+		$this->summaries = $summaries;
+	}
+
+	/**
+	 * @param array $keywords
+	 */
+	public function setKeywords(array $keywords): void
+	{
+		$this->keywords = $keywords;
+	}
+
+	function keywords()
+	{
+		$summaries = $this->getSummaries();
+		$keywords = $this->getKeywords();
+
+		foreach ($summaries as $link => $resume) {
+			for ($i = 0; $i < count($keywords); $i++) {
+
+				$search = $keywords[$i];
+				$replace = '<span style="font-size: 200%; font-family: monospace; background: lightgreen">' . $search . '</span>';
+				if ($i === 0) {
+					$subject = $resume;
+				} else {
+					$subject = $query;
+				}
+
+				$query = str_ireplace($search, $replace, $subject, $count);
+
+				if ($count > 0) {
+					$summaries[$link] = $query;
+					$keysFound[] = $search;
+				}
 			}
 		}
+		return $summaries;
 	}
-	return $summaries;
 }
-
-$summaries = keywords($summaries);
-print_r($summaries);
